448ef56b24d1f4988a46df8e45c762f7799eb68b
borecjeborec1 <robingrundel@seznam.cz>
2022-03-24 19:28:18 +0100

    pause & play

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -12,10 +12,11 @@ dolorum voluptate. Beatae, velit.`
 const vSelect = document.querySelector("#voicesSelect");
 
 let voices = []
+let speech = new SpeechSynthesisUtterance();
 
 let id = setInterval(() => {
   if (voices.length) return clearInterval(id);
-  voices = speechSynthesis.getVoices();
+  voices = window.speechSynthesis.getVoices();
   for (let voice of voices) {
     let option = document.createElement("option");
     option.value = voice.name;
@@ -23,3 +24,38 @@ let id = setInterval(() => {
     vSelect.appendChild(option);
   }
 }, 500)
+
+const playBtn = document.querySelector(".playBtn");
+playBtn.addEventListener("click", () => {
+  if (playBtn.className.includes("fa-play")) {
+    playBtn.className = playBtn.className.replace("fa-play", "fa-pause");
+    if (playBtn.className.includes("start"))
+      return window.speechSynthesis.resume();
+    speech.text = textarea.value;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    speech.voice = voices.find(voice => voice.name === vSelect.value);
+    window.speechSynthesis.speak(speech);
+  } else {
+    window.speechSynthesis.pause();
+    playBtn.className = playBtn.className.replace("fa-pause", "fa-play start");
+  }
+
+})
+
+const stopBtn = document.querySelector(".fa-stop");
+stopBtn.addEventListener("click", () => {
+  window.speechSynthesis.cancel();
+})
+
+
+const downloadBtn = document.querySelector(".fa-download");
+downloadBtn.addEventListener("click", () => {
+  let text = textarea.value;
+  let filename = prompt("Enter file name", "my-file");
+  let link = document.createElement("a");
+  link.href = `data:audio/mp3,${encodeURIComponent(text)}`;
+  link.download = `${filename}.mp3`;
+  link.click();
+})
